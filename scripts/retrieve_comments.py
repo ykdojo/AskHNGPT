2eee7c8e67d2d98539ccf77598688cd83f7bc901
YK <1811651+ykdojo@users.noreply.github.com>
2024-03-05 18:04:20 -0800

    Tested save_ids_to_file and load_ids_from_file

--- scripts/retrieve_comments.py
+++ scripts/retrieve_comments.py
@@ -29,12 +29,3 @@ def get_latest_comments():
 latest_comments = get_latest_comments()
 for comment in latest_comments:
     print(comment)
-
-def save_ids_to_file(id1, id2, filename="ids.txt"):
-    with open(filename, 'w') as file:
-        file.write(f"{id1}\n{id2}")
-
-def load_ids_from_file(filename="ids.txt"):
-    with open(filename, 'r') as file:
-        id1, id2 = file.read().splitlines()
-    return int(id1), int(id2)
--- scripts/test_datastax.py
+++ scripts/test_datastax.py
@@ -27,6 +27,15 @@ import requests
 from FlagEmbedding import BGEM3FlagModel
 model = BGEM3FlagModel('BAAI/bge-m3', use_fp16=True)
 
+def save_ids_to_file(id1, id2, filename="ids.txt"):
+    with open(filename, 'w') as file:
+        file.write(f"{id1}\n{id2}")
+
+def load_ids_from_file(filename="ids.txt"):
+    with open(filename, 'r') as file:
+        id1, id2 = file.read().splitlines()
+    return int(id1), int(id2)
+
 for current_id in range(39610861, 39610871):  # From 39610861 to 39610870
     item_url = f"https://hacker-news.firebaseio.com/v0/item/{current_id}.json"
     item = requests.get(item_url).json()
