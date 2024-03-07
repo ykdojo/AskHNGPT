786c1f67aabcf11153c6872fc8f1cfba595d561f
YK <1811651+ykdojo@users.noreply.github.com>
2024-03-06 17:44:00 -0800

    Add vector_find to run_bge_m3.py

--- scripts/run_bge_m3.py
+++ scripts/run_bge_m3.py
@@ -7,15 +7,15 @@ sentences_1 = ["What is BGE M3?", "Defination of BM25"]
 sentences_2 = ["BGE M3 is an embedding model supporting dense retrieval, lexical matching and multi-vector interaction.", 
                "BM25 is a bag-of-words retrieval function that ranks a set of documents based on the query terms appearing in each document"]
 
-embeddings_2 = model.encode(sentences_2[0])['dense_vecs']
+question = 'What do you think about OpenAI and Elon Musk'
+embeddings_2 = model.encode(question)['dense_vecs']
 
 def compare_sentences(sentence1, sentence2):
     embedding_1 = model.encode(sentence1)['dense_vecs']
     embedding_2 = model.encode(sentence2)['dense_vecs']
     return embedding_1 @ embedding_2
 
-import pdb; pdb.set_trace()
-print(embeddings_2)
+# print(embeddings_2.tolist())
 
 # embeddings_1 = model.encode(sentences_1, 
 #                             batch_size=12, 
@@ -24,3 +24,39 @@ print(embeddings_2)
 # similarity = embeddings_1 @ embeddings_2.T
 # print(similarity)
 # [[0.6265, 0.3477], [0.3499, 0.678 ]]
+
+from astrapy.db import AstraDB, AstraDBCollection
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize the client
+db = AstraDB(
+  token=os.getenv('ASTRA_DB_APPLICATION_TOKEN'),
+  api_endpoint=os.getenv('ASTRA_DB_API_ENDPOINT')
+)
+
+# Create a collection. The default similarity metric is "cosine".
+# collection = db.create_collection("hn_comments_main", dimension=1024, metric="cosine")
+# print(collection)
+# print(f"Connected to Astra DB: {db.get_collections()}")
+
+# Or you can connect to an existing collection directly
+collection = AstraDBCollection(
+    collection_name="hn_comments_main", astra_db=db
+)
+print(collection)
+print(f"Connected to Astra DB: {db.get_collections()}")
+
+# Use vector search with embeddings_2
+documents = collection.vector_find(
+    embeddings_2,
+    limit=100,
+)
+
+# > documents[0].keys()
+# dict_keys(['_id', 'URL', 'user', 'text', '$vector', '$similarity'])
+
+print(documents)
